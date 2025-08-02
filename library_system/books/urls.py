8593f3638a0e96cf74ef2196a0b02c4d87afd3f7
Madhura Kadam <madhurakadam@gmail.com>
2025-08-02 19:58:02 +0530

    Added book availability check API

--- library_system/books/urls.py
+++ library_system/books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet, BookViewSet, BorrowDEtailsViewSet
+from .views import StudentViewSet, BookViewSet, BorrowDEtailsViewSet, get_book_list_for_particulat_student, student_borrow_limit,isbook_availiable
 
 router = DefaultRouter()
 router.register(r'student', StudentViewSet, basename='students')
@@ -9,4 +9,9 @@ router.register(r'borrow_details', BorrowDEtailsViewSet, basename='borrow_detail
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('borrowedbooks/<int:student_id>', get_book_list_for_particulat_student),
+    path('student/<int:student_id>/borrow-limit/', student_borrow_limit),
+    path('student/<int:book_id>/is_avail/', isbook_availiable),
+
+
 ]
--- library_system/books/views.py
+++ library_system/books/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serilizers import Studentserializer, Bookserializers, BorrowdetailsSerializers
 from .models import Student, Book, Borrow_details
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
@@ -14,3 +16,23 @@ class BookViewSet(viewsets.ModelViewSet):
 class BorrowDEtailsViewSet(viewsets.ModelViewSet):
     queryset = Borrow_details.objects.all()
     serializer_class = BorrowdetailsSerializers
+
+@api_view(['GET'])
+def get_book_list_for_particulat_student(request, student_id):
+    student = Student.objects.get(id=student_id)
+    borrowed_book = Borrow_details.objects.filter(student_id=student, returned_date__isnull=True)
+    serializer = BorrowdetailsSerializers(borrowed_book, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def student_borrow_limit(request, student_id):
+    student = Student.objects.get(id=student_id)
+    borrowed_count = Borrow_details.objects.filter(student_id=student, returned_date__isnull=True).count()
+    return Response({"borrow_limit": borrowed_count})
+
+@api_view(['GET'])
+def isbook_availiable(request, book_id):
+    book = Book.objects.get(id=book_id)
+    isavaliable = Borrow_details.objects.filter(is_returned=True)
+    # serializer = BorrowdetailsSerializers(isavaliable)
+    return Response(isavaliable)
